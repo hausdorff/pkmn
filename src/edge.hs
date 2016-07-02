cfa5a0a3129dd9743dde4b707c81334cf265680e
Alex Clemmer <clemmer.alexander@gmail.com>
2016-07-02 12:29:57 -0700

    Add edges

---
+++ src/edge.hs
@@ -0,0 +1,23 @@
+module Edge (
+      Edge(..)
+    , makeEdge
+    , unify
+    )where
+
+import MapSquare
+import Point
+import Transition
+
+data Edge = Edge Point Point Transition
+    deriving (Show)
+
+makeEdge :: Point -> Point -> Transition -> Maybe Edge
+makeEdge from to direction = case (sqr from, sqr to) of
+    (Wall, _) -> Nothing
+    (_, Wall) -> return $ Edge from from direction
+    _ -> return $ Edge from to direction
+
+unify :: Edge -> Edge -> Edge
+unify (Edge p1 p2 trans) (Edge _ _ trans') = Edge p1 p2 (trans <|> trans')
+
+sqr (Point _ _ sqr) = sqr
